0c5b7bdab4688d841078a7c89dfef768bcf2215f
Emre Yıldırım <Emreyildirimbro@gmail.com>
2022-11-26 19:29:37 +0300

    veri-cekme-axios

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import React,{useState,useEffect} from "react";
+
+function App() {
+
+  const [veri,setVeri] = useState("");
+  const [id,setId] = useState(0);
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+        .then(res => setVeri(res.data[id].body))
+        .catch(err=>console.log(err))
+  }, [veri, id] )
+
+  return (
+    <div className="App">
+        <h1>{veri}</h1>
+        <p>
+          <input type="text" value={id} onChange={e =>setId(e.target.value)}/>
+        </p>
+    </div>
+  );
+}
+
+export default App;
